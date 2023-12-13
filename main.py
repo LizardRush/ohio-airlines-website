1ea00b253e8d12233190a97f550d9f0bf5c69fcc
Lizardlock1223 <4836683-Lizardlock1223@users.noreply.replit.com>
2023-12-13 22:16:52 +0000

    2.0, added more stuff

--- main.py
+++ main.py
@@ -1,50 +1,91 @@
-# importing required stuff
+# importing stuff
 import os
-# code
-admin_logged_in = False
+import random
+import json
+# json stuff
+# Dictionary mapping rank IDs to roles
+rank_roles = {
+    1: "Owner",
+    2: "Co-Owner",
+    3: "Loyalty Palace",
+    4: "Ultra Coder",
+    5: "Head of FBI",
+    6: "FBI",
+    7: "Head of Security",
+    8: "Security"
+}
+
+# Read the JSON file
+with open('json/memberRanks.json') as file:
+    data = json.load(file)
+
+# Prepare a dictionary to store members according to their ranks
+members_by_rank = {}
+for member_id, member_info in data.items():
+    rank_id = member_info['rankID']
+    name = member_info['name']
+    role = rank_roles.get(rank_id, f"Rank ID: {rank_id}")  # If a rank ID is not in the mapping, display its ID
+    if rank_id not in members_by_rank:
+         members_by_rank[rank_id] = []
+    members_by_rank[rank_id].append(f"{name}")
+
+# more json stuff
+with open('json/alliedCompanies.json') as companies_file:
+    companies_data = json.load(companies_file)
+
+# Extract the companies from the data
+allied_companies = companies_data.get('companies', [])
+# variables
+admin = False
 running_command = False
+
+# code
 def process_action(number):
-	running_command = True
-	# 1st action
-	if number == "1":
-		print("Owner: Nixon")
-		print("Co-Owner: ???")
-		print("Loyalty palace: Cecelia")
-		print("Christmas Ultra Programmer: Edgar")
-		print("Head of FBI: Kushal")
-		print("???: ???")
-		print("???: ???")
-		print("???: ???")
-	# 2nd action
-	elif number =="2":
-		print("Description:")
-		print("Idk what it is yet")
-	# 3rd action
-	elif number =="3":
-		print("Lizard Co.")
-		print("Dino Co.")
-		print("GimKit Frenzy")
-	elif number =="4":
-		print("https://github.com/LizardRush/ohio-airlines-python-website")
-	elif number =="5":
-		print("replit.com/@lizardlock1223")
-	elif number =="6":
-		print("")
-	else:
-		print("Command Is Not Defined")
-	running_command = False
-	print("—————————————————————————————————")
+    os.system("clear")
+    global running_command
+    running_command = True
+    # 1st action
+    if number == "1":
+         for rank_id, members in members_by_rank.items():
+            if members:  # Check if there are members for the role
+                role = rank_roles.get(rank_id, f"Rank ID: {rank_id}")
+                print(f"{role}: {', '.join(members)}")
+    # 2nd action
+    elif number == "2":
+        print("Description:")
+        print("Idk what it is yet")
+    # 3rd action
+    elif number == "3":
+        for company in allied_companies:
+           print(company)
+    elif number == "4":
+        print("https://github.com/LizardRush/ohio-airlines-python-website")
+    elif number == "5":
+        print("replit.com/@lizardlock1223")
+    elif number == "6":
+        print("")
+    elif number == "7" and admin == True:
+        chosen = random.randint(1, 3)
+        if chosen == 1:
+            print("chosen 1")
+    else:
+        print("Command Is Not Defined")
+    running_command = False
+
 # welcome line
 print("Welcome To The Ohio Airlines Python Website")
 print("This Python Website Is Made By Lizard Rush (Edgar)")
 # name login
 name = input("What is your name: ")
-if name == "Edgar" or name == "Nixon" or name == "Sebastian" or name == "Cecelia":
-	userinputpin = input("it seems like you are signing in with a name of an admin, enter your admin acess pin: ")
-	if userinputpin == os.getenv("ADMIN_PIN"):
-		admin_logged_in = False
+if name in ["Edgar", "Nixon", "Sebastian", "Cecelia"]:
+    userinputpin = input("It seems like you are signing in with the name of an admin, enter your admin access pin: ")
+    if userinputpin == os.getenv("ADMIN_PIN"):
+         admin = True
+
 while True:
 	if not running_command:
+		input("press enter/return to continue")
+		os.system("clear")
 		print("action pane for", name)
 		print("ohio airlines info")
 		print("1: view roles with members")
@@ -54,5 +95,10 @@ while True:
 		print("4: open GitHub Repository")
 		print("5: open lizard rush's replit account (lizardlock1223)")
 		print("6: open lizard rush's github account")
+		print("ohio airlines")
+		print("7: minigame randomizer")
+		if admin:
+			print("admin commands")
+			print("8: view sign ups")
 		userinput = input("Input the number of the action: ")
 		process_action(userinput)
